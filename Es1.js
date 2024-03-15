4207a3d472d4b2046eb47655a5e0207b9b1ba6c0
Alessandro Zappatore <alessandrozappatore03@gmail.com>
2024-03-15 11:23:53 +0100

    Es1  finiched

--- Es1.js
+++ Es1.js
@@ -1,23 +1,25 @@
 "use strict" ;
-let voti=[30, 28, 21, 18, 22, 27];
+const voti=[18, 21, 26, 28, 30, 28, 30];
 let copy=Array.from(voti);
-let x=copy.shift();
-let y=copy.shift();
-console.log("Array originale = "+voti);
+const votiOrdine=[18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30];
 
-let votiOrdine=[18,19,20,21,22,23,24,25,26,27,28,29,30];
-console.log(votiOrdine);
-console.log("Array copia = "+copy);
+const i=12-votiOrdine.indexOf(voti[0]);
+const j=12-votiOrdine.indexOf(voti[1]);
+
+copy.splice(0, 2, votiOrdine[i], votiOrdine[j]);
+
+let res="";
 let sum=0;
-for(let element of voti){
+for(const element of voti){
+    res+=`${element} `;
     sum+=element;
 }
-let med1=sum/voti.length;
+console.log("Voti originali: "+res+"Media: "+Math.round(sum/voti.length));
 
-let sum2=0;
-for(let element2 of copy){
-    sum2+=element2;
+res="";
+sum=0;
+for(const element of copy){
+    res+=`${element} `;
+    sum+=element;
 }
-let med2=sum2/copy.length;
-console.log("Media primo vettore = "+Math.round(med1));
-console.log("Media del vettore modificato = "+Math.round(med2));
+console.log("Voti modificati: "+res+"Media: "+Math.round(sum/copy.length));

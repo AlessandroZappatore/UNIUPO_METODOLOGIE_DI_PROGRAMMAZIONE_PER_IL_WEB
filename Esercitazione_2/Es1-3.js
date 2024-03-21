9bceeb048aba9fbd121adafa157de012fe4c26db
Alessandro Zappatore <alessandrozappatore03@gmail.com>
2024-03-21 16:13:08 +0100

    Es2 esercitazione 2 fatto

--- Esercitazione_2/Es1-3.js
+++ Esercitazione_2/Es1-3.js
@@ -4,9 +4,24 @@ const { format, isValid } = require("date-fns");
 
 function printMenu() {
   console.log("1. Inserire un nuovo task");
-  console.log("2. Rimuovere un task");
-  console.log("3. Mostrare tutti i task esistenti, in ordine alfabetico");
-  console.log("4. Chiudere il programma");
+  console.log("2. Rimuovere i task in base alla descrizione");
+  console.log("3. Rimuovere i task in base alla data")
+  console.log("4. Mostrare tutti i task esistenti, in ordine alfabetico");
+  console.log("5. Chiudere il programma");
+}
+
+function inputDate(){
+  let data = null;
+  do {
+    data = readline.question(
+      "Inserire la data (YYYY-MM-DD) (oppure lasciare vuoto per nessuna scadenza): ");
+    if (data && !isValid(new Date(data))) {
+      console.error(
+        "Formato data non valido. Inserire nel formato YYYY-MM-DD oppure lasciare vuoto per nessuna scadenza.");
+    }
+  } while (data && !isValid(new Date(data)));
+  const formattedDate = data ? format(new Date(data), "yyyy-MM-dd") : null;
+  return formattedDate;
 }
 
 function addTask(tasks) {
@@ -20,32 +35,19 @@ function addTask(tasks) {
 
   const visibilita = readline.keyInYNStrict("Il task e' privato?: ");
 
-  let data = null;
-  do {
-    data = readline.question(
-      "Inserire la deadline (YYYY-MM-DD) (oppure lasciare vuoto per nessuna scadenza): "
-    );
-    if (data && !isValid(new Date(data))) {
-      console.error(
-        "Formato data non valido. Inserire nel formato YYYY-MM-DD oppure lasciare vuoto per nessuna scadenza."
-      );
-    }
-  } while (data && !isValid(new Date(data)));
-  const formattedDate = data ? format(new Date(data), "yyyy-MM-dd") : null;
+  const data=inputDate();
 
   let task = {
     descption: descrizione,
     important: importanza,
     private: visibilita,
-    deadline: formattedDate,
+    deadline: data,
   };
   tasks.push(task);
 }
 
 function deleteTaskByDescription(tasks) {
-  let descrizione = readline.question(
-    "Inserire la descrizione del task da eliminare: "
-  );
+  let descrizione = readline.question("Inserire la descrizione del task da eliminare: ");
   let toBeRemoved = [];
   for (let task of tasks) {
     if (task.descption === descrizione) {
@@ -57,6 +59,21 @@ function deleteTaskByDescription(tasks) {
     tasks.splice(tasks.indexOf(element), 1);
   }
 }
+
+function deleteTaskByDate(tasks){
+    const deleteData=inputDate();
+    let toBeRemoved=[];
+    for(let task of tasks){
+      if(task.deadline === deleteData){
+        toBeRemoved.push(task);
+      }
+    }
+
+    for(let element of toBeRemoved){
+      tasks.splice(tasks.indexOf(element), 1);
+    }
+}
+
 function printTasks(tasks) {
   tasks.sort((a, b) => a.descption.localeCompare(b.descption));
   console.log(tasks);
@@ -76,9 +93,12 @@ do {
       deleteTaskByDescription(tasks);
       break;
     case "3":
+      deleteTaskByDate(tasks);
+      break;
+    case "4":
       printTasks(tasks);
       break;
     default:
       break;
   }
-} while (choice != 4);
+} while (choice != 5);

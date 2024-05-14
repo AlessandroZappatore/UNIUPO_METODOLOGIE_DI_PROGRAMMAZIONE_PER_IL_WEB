db0d90ddc0cbfd2ab4d5ac4374c5d9cc31ab0857
Alessandro Zappatore <alessandrozappatore03@gmail.com>
2024-05-14 14:52:14 +0200

    A

--- Lab-Express-starter pack/L04-Express-starter pack/server.js
+++ Lab-Express-starter pack/L04-Express-starter pack/server.js
@@ -3,11 +3,8 @@
 const express = require('express') ;
 const morgan = require('morgan');
 const Task = require('./task');
-const TaskManager = require('./taskManager');
 //validation middle-ware
 const {check, validationResult} = require('express-validator');
-
-const tm = new TaskManager();
 
 
 // create application
@@ -22,56 +19,6 @@ app.use(morgan('tiny'));
 
 // process body content as JSON
 app.use(express.json());
-
-
-
-//get task with id
-app.get('/tasks/:id', (req, res) => { 
-
-    tm.getTask(req.params.id).then( (task) => {
-        if(task.error){
-            res.status(404).json(task);
-        } else {
-            res.json(task);
-        }}).catch( (err) => {
-
-           res.status(500).json({ 
-               'errors': [{'param': 'Server', 'msg': err}],
-            }); 
-        } )
- 
-   
-});
-
-app.post('/tasks',  (req, res) => {
-    //modificare inserendo validazione dei parametri
-    const task = req.body;
-    console.log(task);
-
-    tm.addTask(task).then( (id) => {
-
-        if (id.error) {
-
-        }
-        else{
-            //modificare inserendo nell'header l'id della nuova risorsa
-            // e.g., tasks/2
-            res.status(200).end();
-
-        }
-
-
-    }).catch((err) =>{
-
-        res.status(500).json({ 
-            'errors': [{'param': 'Server', 'msg': err}],
-         }); 
-
-    });
-    
-    
-})
-
 
 // activate server
 app.listen (port, () =>  console.log(`Server ready running at port ${port}` )) ;

32b79a935537a7ae913759fc8d77acf1807b37fb
Alessandro Zappatore <alessandrozappatore03@gmail.com>
2024-05-20 11:05:02 +0200

    Database popolato

--- Progetto/WIW_db.sql
+++ Progetto/WIW_db.sql
@@ -21,23 +21,39 @@
 --     Dove_vederlo, 
 --     Trama, 
 --     Poster BLOB);
-CREATE TABLE profilo(
-    utente, 
-    contenuto,
-    PRIMARY KEY(utente, contenuto),
-    FOREIGN KEY(utente) REFERENCES utente(email),
-    FOREIGN KEY(contenuto) REFERENCES contenuto(id_contenuto));
-CREATE TABLE rating(
-    utente, 
-    contenuto, 
-    Voto INTEGER CHECK(Voto>=0 AND Voto<=5),
-    PRIMARY KEY(utente, contenuto),
-    FOREIGN KEY(utente) REFERENCES utente(email),
-    FOREIGN KEY(contenuto) REFERENCES contenuto(id_contenuto));
-CREATE TABLE commento(
-    utente, 
-    contenuto, 
-    Testo,
-    PRIMARY KEY(utente, contenuto),
-    FOREIGN KEY(utente) REFERENCES utente(email),
-    FOREIGN KEY(contenuto) REFERENCES contenuto(id_contenuto));  
+-- CREATE TABLE profilo(
+--     utente, 
+--     contenuto,
+--     PRIMARY KEY(utente, contenuto),
+--     FOREIGN KEY(utente) REFERENCES utente(email),
+--     FOREIGN KEY(contenuto) REFERENCES contenuto(id_contenuto));
+-- CREATE TABLE rating(
+--     utente, 
+--     contenuto, 
+--     Voto INTEGER CHECK(Voto>=0 AND Voto<=5),
+--     PRIMARY KEY(utente, contenuto),
+--     FOREIGN KEY(utente) REFERENCES utente(email),
+--     FOREIGN KEY(contenuto) REFERENCES contenuto(id_contenuto));
+-- CREATE TABLE commento(
+--     utente, 
+--     contenuto, 
+--     Testo,
+--     PRIMARY KEY(utente, contenuto),
+--     FOREIGN KEY(utente) REFERENCES utente(email),
+--     FOREIGN KEY(contenuto) REFERENCES contenuto(id_contenuto));  
+
+-- INSERT INTO utente(email, Nome, Cognome, Data_nascita, Nome_utente, Tipologia, Password)
+-- VALUES ('utente1@gmail.com', 'Utente', 'Uno', '10/3/2000', 'Utente 1', 0, 'password');
+
+-- INSERT INTO contenuto(id_contenuto, Titolo, Tipologia, Genere, Registi, Attori, Data_Uscita, Durata, Dove_vederlo, Trama)
+-- VALUES ('Film1','Interstellar', 0, 'Fantascienza', 'Christopher Nolan', 'Matthew McConaughey, Anne Hathaway, Michael Caine, John Lithgow, Jessica Chastain, Casey Affleck, Mackenzie Foy, Wes Bentley, Matt Damon, Timothée Chalamet, William Devane, David Oyelowo, Bill Irwin',
+--  '5/11/2014', 169, 'NOW TV', 'L''umanità è sull''orlo dell''estinzione e un gruppo di astronauti viaggia attraverso un wormhole alla ricerca di un altro pianeta abitabile.'); 
+
+-- INSERT INTO profilo(utente, contenuto)
+-- VALUES ('utente1@gmail.com', 'Film1');
+
+-- INSERT INTO rating(utente, contenuto, voto)
+-- VALUES ('utente1@gmail.com', 'Film1', 4);
+
+INSERT INTO commento(utente, contenuto, testo)
+VALUES ('utente2@gmail.com', 'Film1', 'Film molto bello');

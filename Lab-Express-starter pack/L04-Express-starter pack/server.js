94e27d1ed19c1fbb5b809fd8192eb70d3e65f94e
Alessandro Zappatore <alessandrozappatore03@gmail.com>
2024-05-21 15:37:20 +0200

    Lezione 21 maggio 2024, Da rivedere

--- Lab-Express-starter pack/L04-Express-starter pack/server.js
+++ Lab-Express-starter pack/L04-Express-starter pack/server.js
@@ -1,11 +1,13 @@
-"use strict";
+'use strict';
 // import package
-const express = require("express");
-const morgan = require("morgan");
-const Task = require("./task");
-const tm = require("./dao");
+const express = require('express') ;
+const morgan = require('morgan');
+const Task = require('./task');
+const dao = require('./dao');
 //validation middle-ware
-const { check, validationResult } = require("express-validator");
+const {check, validationResult} = require('express-validator');
+
+
 
 // create application
 const app = express();
@@ -13,142 +15,173 @@ const app = express();
 //set port
 const port = 3000;
 
+
 // set-up logging
-app.use(morgan("tiny"));
+app.use(morgan('tiny'));
 
 // process body content as JSON
 app.use(express.json());
+app.use(express.static('public'));//public è la cartella
 
-//Recuperare la lista di tutti i task disponibili.
-app.get("/tasks", (req, res) => {
-  tm.getTasks()
-    .then((tasks) => {
-      if (tasks.error) {
-        res.status(404).json(tasks);
-      } else {
-        res.json(tasks);
-      }
-    })
-    .catch((err) => {
-      res.status(500).json({
-        'errors': [{'param': 'Server', 'msg': err}]
-      });
-    });
+
+app.use(express.static('public'));
+app.get('/', (req, res) => res.redirect('/index.html'));
+
+
+/* API REST */
+//get all tasks
+app.get ('/api/tasks', (req, res) => {
+    const filter = req.query.filter;
+    dao.getTasks(filter).then (( tasks) => {
+        if (tasks.error){
+            res.status(404).json(task);
+        } else {
+            res.json(tasks);
+        }}).catch( (err) => {
+
+           res.status(500).json({ 
+               'errors': [{'param': 'Server', 'msg': err}],
+            }); 
+        } )    
+  
+  
 });
 
-//Recuperare un singolo task, dato il suo id
-app.get("/tasks/:id", (req, res) => {
-  tm.getTask(req.params.id)
-    .then((task) => {
-      if (task.error) {
-        res.status(404).json(task);
-      } else {
-        res.status(200).json(task);
-      }
-    })
-    .catch((err) => {
-      res.status(500).json({
-        'errors': [{'param': 'Server', 'msg': err}]
-      });
-    });
+
+//get task with id
+app.get('/api/tasks/:id', (req, res) => { 
+
+    dao.getTask(req.params.id).then( (task) => {
+        if(task.error){
+            res.status(404).json(task);
+        } else {
+            res.json(task);
+        }}).catch( (err) => {
+
+           res.status(500).json({ 
+               'errors': [{'param': 'Server', 'msg': err}],
+            }); 
+        } )
+ 
+   
 });
 
-//Cancellare un task, dato il suo id
-app.delete("/tasks/:id", (req, res) => {
-  const id = req.params.id;
-  tm.deleteTask(req.params.id)
-    .then((result) => {
-      res.status(204).end();
-    })
-    .catch((err) => {
-      res.status(500).json({
-        'errors': [{'param': 'Server', 'msg': err}]
-      });
-    });
-});
 
-//Creare un nuovo task, fornendo tutte le informazioni rilevanti (eccetto l’id)
-app.post(
-  "/tasks",
-  [
-    check("description").isLength({ min: 5 }),
-    check("description").notEmpty(),
-    check("private").isBoolean(),
-    check("important").isBoolean(),
-    check("completed").isBoolean(),
-    check("deadline").isDate(),
-    check("projectName").isString(),
-  ],
-  (req, res) => {
+//add new task
+app.post('/api/tasks', [
+    
+    check('description').isLength({min: 5}),
+    check('privateTask').isBoolean(),
+    check('important').isBoolean(),
+    check('project').isString(),
+    check('deadline').isString(),
+    check('completed').isBoolean()
+],
+(req, res) => {
+
     console.log(req);
     const errors = validationResult(req);
 
     if (!errors.isEmpty()) {
-      return res.status(422).json({ errors: errors.array() });
+        return res.status(422).json({errors: errors.array()});
+ 
     }
+    const task = req.body;
 
-    tm.addTask(req.body)
-      .then((id) => {
-        res.status(201).header("Location", `/tasks/${id}`).end();
-      })
-      .catch((err) => {
+    dao.addTask(task).then((id) => {
+        // 201 -> Created 
+        // Location Header redirects to the newly reported response
+        res.status(201).json({'id': id});
+        
+    }).catch((err) =>{
+         res.status(500).json({ 
+            'errors': [{'param': 'Server', 'msg': err}],
+         }); 
+    } );
+   
+});
+
+// update a task
+app.put('/api/tasks/:id' , [
+    
+    check('description').isLength({min: 5}),
+    //check('description').notEmpty(),
+    check('privateTask').isBoolean(),
+    check('important').isBoolean(),
+    check('project').isString(),
+    check('deadline').isString(),
+    check('completed').isBoolean()
+],  (req, res) => {
+   
+
+        console.log(req);
+        const errors = validationResult(req);
+    
+        if (!errors.isEmpty()) {
+            return res.status(422).json({errors: errors.array()});
+     
+        }
+        const task = req.body;
+    
+        dao.updateTask(req.params.id, task).then((err) => {
+            if (err)
+                res.status(404).json(err);
+            else
+                res.status(200).end();
+
+            
+        }).catch((err) =>{
+             res.status(500).json({ 
+                'errors': [{'param': 'Server', 'msg': err}]
+             }); 
+        } );
+
+    } );
+   
+
+// set a task as completed
+app.patch('/api/tasks/:id/completed' ,  (req, res) => {
+    dao.setCompleted(req.params.id).then( (err) => {
+        if (err)
+            res.status(404).json(err);
+        else
+            // 204: Succesfull request with no additional content
+            res.status(204).end();
+    }).catch((err) =>{
         res.status(500).json({
-          'errors': [{'param': 'Server', 'msg': err}]
+            'errors': [{'param': 'Server', 'msg': err}]
         });
-      });
-  }
-);
-
-//Segnare un task come “completato”
-app.patch("/tasks/:id/completed", (req, res) => {
-  tm.setCompleted(req.params.id)
-    .then((err) => {
-      if (err) {
-        res.status(404).json(err);
-      } else {
-        res.status(204).end();
-      }
-    })
-    .catch((err) => {
-      res.status(500).json({
-        'errors': [{'param': 'Server', 'msg': err}]
-      });
     });
+
+
+}) ;
+
+
+
+
+
+
+//delete a task
+
+app.delete('/api/tasks/:id', (req,res)=> {
+    const id = req.params.id;
+
+    dao.deleteTask(req.params.id).then( (result) => {
+            // 204 -> No content
+            res.status(204).end();
+        }).catch( (err) => {
+
+           res.status(500).json({ 
+               'errors': [{'param': 'Server', 'msg': err}],
+            }); 
+        } )
+
+})
+
+// All requests will be served by our client-side app
+//Da mettere in fondo!!!
+app.get('*', function (request, response) {
+  response.sendFile(path.resolve(__dirname, 'public/index.html'));
 });
-
-//Aggiornare un task esistente, fornendo tutte le informazioni rilevanti (tutte le proprietà tranne l’id
-//sovrascriveranno le proprietà correnti del task esistente con lo stesso ‘id’)
-app.put("/tasks/:id",[
-  check("description").isLength({ min: 5 }),
-  check("description").notEmpty(),
-  check("private").isBoolean(),
-  check("important").isBoolean(),
-  check("completed").isBoolean(),
-  check("deadline").isDate(),
-  check("projectName").isString(),
-], (req, res) =>{
-  console.log(req);
-  const errors = validationResult(req);
-
-  if(!errors.isEmpty()){
-    return res.status(422).json({errors: errors.array()});
-  }
-
-  tm.updateTask(req.params.id, req.body).then((err) =>{
-    if(err){
-      res.status(404).json(err);
-    }
-    else{
-      res.status(200).end();
-    }
-  }).catch((err) =>{
-    res.status(500).json({
-      'errors': [{'param': 'Server', 'msg': err}]
-    });
-  });
-});
-
-
-
 // activate server
-app.listen(port, () => console.log(`Server ready running at port ${port}`));
+app.listen (port, () =>  console.log(`Server ready running at port ${port}` )) ;

e1ee82612e5379f2a54f8568adc7e24dc2dbe1a0
Alessandro Zappatore <alessandrozappatore03@gmail.com>
2024-07-22 16:38:25 +0200

    Gestione pagina inesistente

--- ProgettoDefinitivo/Progetto_ZappatoreAlessandro/app.js
+++ ProgettoDefinitivo/Progetto_ZappatoreAlessandro/app.js
@@ -144,7 +144,7 @@ app.use(function (err, req, res, next) {
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error", { message: "Errore", buttonAction: "Home" });
 });
 
 module.exports = app;
